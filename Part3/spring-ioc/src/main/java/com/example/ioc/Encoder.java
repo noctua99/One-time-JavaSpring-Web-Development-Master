0fbc0a9b69da6bd849e6a3c0349004661e9a438c
jongi <jonginchoi99@gmail.com>
2022-07-25 20:10:19 +0900

    AOP

--- Part3/spring-ioc/src/main/java/com/example/ioc/Encoder.java
+++ Part3/spring-ioc/src/main/java/com/example/ioc/Encoder.java
@@ -3,12 +3,11 @@ package com.example.ioc;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
 public class Encoder {
 
     private IEncoder iEncoder;
 
-    public Encoder(@Qualifier("urlEncoder") IEncoder iEncoder) {
+    public Encoder( IEncoder iEncoder) {
         this.iEncoder = iEncoder;
     }
 
