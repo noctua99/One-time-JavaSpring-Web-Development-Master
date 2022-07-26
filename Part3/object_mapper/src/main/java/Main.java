f286cafa6191e4a2aca1637a62912c4509f7160e
jongi <jonginchoi99@gmail.com>
2022-07-26 20:07:23 +0900

    Object Mapper

---
+++ Part3/object_mapper/src/main/java/Main.java
@@ -0,0 +1,34 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dto.Car;
+import dto.User;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("main");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        User user = new User();
+        user.setName("홍길동");
+        user.setAge(10);
+
+        Car car1 = new Car();
+        car1.setName("K5");
+        car1.setCarNumber("11가 1111");
+        car1.setType("sedan");
+
+        Car car2 = new Car();
+        car2.setName("Q5");
+        car2.setCarNumber("22가 2222");
+        car2.setType("SUV");
+
+        List<Car> carLIst = Arrays.asList(car1, car2);
+        user.setCars(carLIst);
+
+        System.out.println(user);
+    }
+}
